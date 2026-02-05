6e2e9df9346179b6320e5bdc1698b207704046ba
sadimanna <manna.siladittya@gmail.com>
2026-02-05 11:54:48 +0530

    Added gan gradmatching python file

---
+++ breaching/attacks/MODEL_FILES.py
@@ -0,0 +1,10 @@
+STYLEGANXL_CIFAR = "https://s3.eu-central-1.amazonaws.com/avg-projects/stylegan_xl/models/cifar10.pkl"
+STYLEGANXL_IMAGENET32 = "https://s3.eu-central-1.amazonaws.com/avg-projects/stylegan_xl/models/imagenet32.pkl"
+STYLEGANXL_IMAGENET64 = "https://s3.eu-central-1.amazonaws.com/avg-projects/stylegan_xl/models/imagenet64.pkl"
+STYLEGANXL_IMAGENET128 = "https://s3.eu-central-1.amazonaws.com/avg-projects/stylegan_xl/models/imagenet128.pkl"
+STYLEGANXL_IMAGENET256 = "https://s3.eu-central-1.amazonaws.com/avg-projects/stylegan_xl/models/imagenet256.pkl"
+STYLEGANXL_FFHQ256 = "https://s3.eu-central-1.amazonaws.com/avg-projects/stylegan_xl/models/ffhq256.pkl"
+
+STYLEGAN2ADA_CIFAR = "https://nvlabs-fi-cdn.nvidia.com/stylegan2-ada-pytorch/pretrained/cifar10.pkl"
+STYLEGAN2ADA_FFHQ1024 = "https://nvlabs-fi-cdn.nvidia.com/stylegan2-ada-pytorch/pretrained/ffhq.pkl"
+
