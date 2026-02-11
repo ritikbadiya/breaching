26eb89019ab5e9bd73236d60bfba540f4fd8fbec
sadimanna <manna.siladittya@gmail.com>
2026-02-11 11:59:00 +0530

    fixed warning in custom_ops styleganXL

--- breaching/attacks/auxiliaries/stylegan_xl/torch_utils/ops/bias_act.py
+++ breaching/attacks/auxiliaries/stylegan_xl/torch_utils/ops/bias_act.py
@@ -40,6 +40,7 @@ activation_funcs = {
 
 #----------------------------------------------------------------------------
 
+_inited = False
 _plugin = None
 _null_tensor = torch.empty([0])
 
@@ -50,10 +51,11 @@ def _custom_ops_disabled():
     return str(flag).strip().lower() not in ('0', 'false', 'no', '')
 
 def _init():
-    global _plugin
+    global _inited, _plugin
     if _custom_ops_disabled():
         return False
-    if _plugin is None:
+    if not _inited:
+        _inited = True
         try:
             _plugin = custom_ops.get_plugin(
                 module_name='bias_act_plugin',
